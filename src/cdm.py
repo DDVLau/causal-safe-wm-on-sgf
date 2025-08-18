0aa86ffedbf9fadefdeed2300d841ece19ad9513
ddvlau <ddvlau@foxmail.com>
2025-08-18 22:45:55 +0100

    Bug fix: cdm

--- src/cdm.py
+++ src/cdm.py
@@ -10,11 +10,12 @@ import wandb
 
 from replay import ReplayBufferSafeRL
 
-from causallearn.graph.GeneralGraph import GeneralGraph
+from causallearn.graph.Dag import Dag
 from causallearn.graph.GraphNode import GraphNode
 from causallearn.graph.Edge import Edge
 from causallearn.utils.GraphUtils import GraphUtils
 from causallearn.search.ConstraintBased.FCI import fci
+from causallearn.utils.PCUtils.BackgroundKnowledge import BackgroundKnowledge
 
 
 class CausalDynamicModel(ABC):
@@ -27,7 +28,8 @@ class CausalDynamicModel(ABC):
         self.action_dim = action_dim
 
         nodes, nodes_dict = self.init_nodes()
-        self._pdag = GeneralGraph(nodes=nodes)
+        self._pdag = Dag(nodes=nodes)
+        self.bg_knowledge = self.init_background_knowledge()
         self.nodes_dict = nodes_dict
 
     def init_nodes(self) -> List[GraphNode]:
@@ -37,13 +39,29 @@ class CausalDynamicModel(ABC):
         curr_state_nodes = [GraphNode(f"y_{i}") for i in range(state_nodes)]
         next_state_nodes = [GraphNode(f"nexty_{i}") for i in range(state_nodes)]
         action_node = [GraphNode(f"action_{i}") for i in range(self.action_dim)]
-        reward_node = GraphNode("reward")
-        cost_node = GraphNode("cost")
-        nodes = curr_state_nodes + next_state_nodes + action_node + [reward_node, cost_node]
+        reward_node = [GraphNode("reward")]
+        cost_node = [GraphNode("cost")]
+        nodes = curr_state_nodes + action_node + next_state_nodes + reward_node + cost_node
 
         nodes_dict = dict(y=curr_state_nodes, next_y=next_state_nodes, action=action_node, reward=reward_node, cost=cost_node)
 
         return nodes, nodes_dict
+
+    def init_background_knowledge(self) -> BackgroundKnowledge:
+        bg_knowledge = BackgroundKnowledge()
+        # y_{t} -> y_{t} or y_{t+1} -> y_{t+1}, or a_{t+1} -> a_{t+1}
+        bg_knowledge.add_forbidden_by_pattern("y", "y")
+        bg_knowledge.add_forbidden_by_pattern("nexty", "nexty")
+        bg_knowledge.add_forbidden_by_pattern("action", "action")
+        # y_{t+1} -> y_{t}
+        bg_knowledge.add_forbidden_by_pattern("nexty", "y")
+        # r_{t+1} -> y_{t+1} or r_{t+1} -> y_{t}
+        bg_knowledge.add_forbidden_by_pattern("reward", "nexty")
+        bg_knowledge.add_forbidden_by_pattern("reward", "y")
+        # c_{t+1} -> y_{t+1} or c_{t+1} -> y_{t}
+        bg_knowledge.add_forbidden_by_pattern("cost", "nexty")
+        bg_knowledge.add_forbidden_by_pattern("cost", "y")
+        return bg_knowledge
 
     def compute(self, buffer: ReplayBufferSafeRL, wm, seed) -> Dict:
         metrics = {}
@@ -65,8 +83,8 @@ class CausalDynamicModel(ABC):
             y = wm.encoder(o)
             next_y = wm.encoder(next_o)
 
-        fci_input = self.preprocess_data(y, a, next_y, next_r, next_c, 32)
-        new_edges = self._fci_compute(fci_input)
+        fci_input, node_names = self.preprocess_data(y, a, next_y, next_r, next_c, 32)
+        new_edges = self._fci_compute(fci_input, node_names)
         metrics["new_edges"] = new_edges
 
         return metrics
@@ -89,7 +107,7 @@ class CausalDynamicModel(ABC):
         rewards: torch.Tensor,
         costs: torch.Tensor,
         num_variables: int,
-    ) -> torch.Tensor:
+    ) -> Tuple[torch.Tensor, List]:
         y_r, _, y_c, _ = torch.split(y, [self.state_dict["reward"], self.state_dict["reward"], self.state_dict["cost"], self.state_dict["cost"]], dim=-1)
 
         next_y_r, _, next_y_c, _ = torch.split(next_y, [self.state_dict["reward"], self.state_dict["reward"], self.state_dict["cost"], self.state_dict["cost"]], dim=-1)
@@ -102,31 +120,35 @@ class CausalDynamicModel(ABC):
         actions = actions[:, -1, :]
         rewards = rewards.unsqueeze(-1)
         costs = costs.unsqueeze(-1)
-        data = torch.cat((y, actions, next_y, rewards, costs), axis=-1)
-        # (n_samples, n_feature)
-        return data.reshape(-1, data.shape[-1]).numpy().astype(np.float32)
+        data = torch.cat((y, actions, next_y, rewards, costs), axis=-1)  # (n_samples, n_feature)
 
-    def _fci_compute(self, data: np.ndarray) -> int:
+        node_names = (
+            [self.nodes_dict["y"][i].name for i in idx]
+            + [node.name for node in self.nodes_dict["action"]]
+            + [self.nodes_dict["next_y"][i].name for i in idx]
+            + [node.name for node in self.nodes_dict["reward"]]
+            + [node.name for node in self.nodes_dict["cost"]]
+        )
+        return data.reshape(-1, data.shape[-1]).numpy().astype(np.float32), node_names
+
+    def _fci_compute(self, data: np.ndarray, node_names: Dict) -> int:
         """
         Testing only: Compute the FCI algorithm
         """
 
         data = data.astype(np.float32)
-        graph, edges = fci(data, depth=2, max_path_length=3, verbose=False)
-        fci_node_list = graph.get_nodes()
-        dag_node_list = self._pdag.nodes
+        graph, edges = fci(data, depth=2, max_path_length=3, verbose=False, background_knowledge=self.bg_knowledge, node_names=node_names)
 
         ct = 0
         for item in edges:
             node1, node2 = item.get_node1(), item.get_node2()
             # find index
-            index1 = fci_node_list.index(node1)
-            index2 = fci_node_list.index(node2)
+            pdag_node1, pdag_node2 = self._pdag.get_node(node1.name), self._pdag.get_node(node2.name)
 
-            sanity_case = self.sanity_check(dag_node_list[index1], dag_node_list[index2])
+            sanity_case = self.sanity_check(pdag_node1, pdag_node2)
 
             if sanity_case == 0:
-                new_edge = Edge(dag_node_list[index1], dag_node_list[index2], end1=item.get_endpoint1(), end2=item.get_endpoint2())
+                new_edge = Edge(pdag_node1, pdag_node2, end1=item.get_endpoint1(), end2=item.get_endpoint2())
                 # if "dd" in new_edge.properties:
                 #     continue
                 flag = self._pdag.add_edge(new_edge)
@@ -169,7 +191,12 @@ class CausalDynamicModel(ABC):
         return None
 
     @property
-    def nodes(self) -> List[str]:
+    def nodes(self) -> List:
         if self._pdag is None:
             return []
         return self._pdag.get_nodes()
+
+    @property
+    def node_names(self) -> List[str]:
+        names = [n.name for n in self.nodes]
+        return names
